d0a700f612182214025f044c7f4a06965fee8945
Santoshreddy-98 <gorantlasantosh1998@gmail.com>
2023-05-19 17:21:44 +0530

    Build deleted, logs set up for Cloud Watch

--- Loggers/ViewMCQLogger.js
+++ Loggers/ViewMCQLogger.js
@@ -1,4 +1,6 @@
 const {createLogger,transports,format} = require('winston')
+const WinstonCloudWatch = require('winston-aws-cloudwatch');
+ 
 
 const ViewMCQLogger =  createLogger({
     transports:[
@@ -14,7 +16,14 @@ const ViewMCQLogger =  createLogger({
             filename:"viewmcq-error.log",
             level:"error",
             format:format.combine(format.timestamp({format:'YYYY-MM-DD HH:mm:ss'}),format.json())
-        })
+        }),
+        new WinstonCloudWatch({
+            logGroupName: 'P2F-onlineassessment-logs',
+            logStreamName: 'online-assessment-logs-stream',
+            awsRegion: 'ap-south-1', // Replace with your desired AWS region
+            awsAccessKeyId: 'p2f-online-logger-user', // Replace with your AWS access key ID
+            awsSecretKey: 'f7PS7}IP' // Replace with your AWS secret access key
+          })
         
     ]
 })
--- Loggers/logger.js
+++ Loggers/logger.js
@@ -1,4 +1,6 @@
 const {createLogger,transports,format} = require('winston')
+const WinstonCloudWatch = require('winston-aws-cloudwatch');
+ 
 
 
 const Logger =  createLogger({
@@ -15,7 +17,14 @@ const Logger =  createLogger({
             filename:"candidate-error.log",
             level:"error",
             format:format.combine(format.timestamp({format:'YYYY-MM-DD HH:mm:ss'}),format.json())
-        })
+        }),
+        new WinstonCloudWatch({
+            logGroupName: 'P2F-onlineassessment-logs',
+            logStreamName: 'online-assessment-logs-stream',
+            awsRegion: 'ap-south-1', // Replace with your desired AWS region
+            awsAccessKeyId: 'p2f-online-logger-user', // Replace with your AWS access key ID
+            awsSecretKey: 'f7PS7}IP' // Replace with your AWS secret access key
+          })
         
     ]
 })
