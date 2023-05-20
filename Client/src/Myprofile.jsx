6b6f9391f8470ac70048bc8ad56f1757c79b468b
Santoshreddy-98 <gorantlasantosh1998@gmail.com>
2023-05-20 11:47:21 +0530

    Made changes for token expire reload

--- Client/src/Myprofile.jsx
+++ Client/src/Myprofile.jsx
@@ -1,38 +1,60 @@
 import React, { useContext, useState, useEffect } from "react";
-import { Link ,useLocation} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { store } from "./App";
 import { Navigate } from "react-router";
 import axios from "axios";
 import { BASE_URL } from "./Service/helper";
+
 const MyProfile = () => {
   const location = useLocation();
   const email = location.state.email;
   const [token, setToken] = useContext(store);
   const [data, setData] = useState({});
+
   useEffect(() => {
-    axios
-      .get(`${BASE_URL}/myprofile`, {
-        headers: {
-          "x-token": token,
-        },
-      })
-      .then((res) => setData(res.data))
-      .catch((err) => console.log(err));
+    const storedToken = localStorage.getItem("token");
+    if (storedToken) {
+      setToken(storedToken);
+    }
+  }, [setToken]);
+
+  useEffect(() => {
+    if (token) {
+      axios
+        .get(`${BASE_URL}/myprofile`, {
+          headers: {
+            "x-token": token,
+          },
+        })
+        .then((res) => setData(res.data))
+        .catch((err) => console.log(err));
+    }
   }, [token]);
+
+  useEffect(() => {
+    if (token) {
+      localStorage.setItem("token", token);
+    }
+  }, [token]);
+
   if (!token) {
-    return <Navigate to="/login"/>;
+    return <Navigate to="/login" />;
   }
+
   return (
     <div style={{ backgroundColor: "#F0F1F4" }}>
       {data && (
         <center>
           <br />
-          <div className="card" style={{ width: "18rem",marginTop:"90px" }}>
+          <div className="card" style={{ width: "18rem", marginTop: "90px" }}>
             <div className="card-body">
-            <h5 className="card-title">Welcome {email}</h5>
-            <br />
+              <h5 className="card-title">Welcome {email}</h5>
+              <br />
               <button
-                style={{ backgroundColor: "#F19E18", fontFamily: "fantasy" }}
+                style={{
+                  backgroundColor: "#F19E18",
+                  fontFamily: "fantasy",
+                }}
                 className="btn"
                 onClick={() => setToken(null)}
               >
@@ -40,20 +62,21 @@ const MyProfile = () => {
               </button>
             </div>
             <div className="card-body">
-            <Link to="/CandidateList"
-              className="btn"
-              style={{
+              <Link
+                to="/CandidateList"
+                className="btn"
+                style={{
                   marginLeft: "5px",
                   backgroundColor: "#6BD8BA",
                   fontFamily: "fantasy",
-                  marginTop:"2px"
+                  marginTop: "2px",
                 }}
               >
-              Manage Candidate
+                Manage Candidate
               </Link>
             </div>
             <div className="card-body">
-            <Link
+              <Link
                 to="/CandidateForm"
                 className="btn"
                 style={{
@@ -66,7 +89,7 @@ const MyProfile = () => {
               </Link>
             </div>
             <div className="card-body">
-            <Link
+              <Link
                 to="/getAllMCQQuestions"
                 className="btn"
                 style={{
@@ -79,7 +102,7 @@ const MyProfile = () => {
               </Link>
             </div>
             <div className="card-body">
-            <Link
+              <Link
                 to="/AddQuestions"
                 className="btn"
                 style={{
@@ -88,7 +111,7 @@ const MyProfile = () => {
                   fontFamily: "fantasy",
                 }}
               >
-              Add Questions
+                Add Questions
               </Link>
             </div>
           </div>
@@ -97,4 +120,5 @@ const MyProfile = () => {
     </div>
   );
 };
+
 export default MyProfile;
