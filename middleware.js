2aae62960f3b1409c872490a4dd13355f24d6173
Prateek <prateek@p2fsemi.in>
2023-06-01 18:41:04 +0530

    Changes and new build

--- middleware.js
+++ middleware.js
@@ -2,7 +2,8 @@ const jwt = require("jsonwebtoken");
 //jwt for evaluator
 const Evaluatorfunction = (req, res, next) => {
   try {
-    let token = req.header("x-token");
+    let token = req.header("x-token")
+    console.log(token);
     if (!token) {
       return res.status(400).send("Token Not found");
     }
