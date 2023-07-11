fc518647f00fcdaec86b299437615679e2415e79
Prateek <prateek@p2fsemi.in>
2023-07-11 10:59:45 +0530

    Change

--- Client/src/Nav.jsx
+++ Client/src/Nav.jsx
@@ -43,7 +43,7 @@ const Nav = () => {
         }}
         className="navbar-brand"
       >
-        P2F Online Assessment  
+        Online Assessment  
       </span>
       <div className="d-flex">
         {showHomeLink && (
